a3482f9881021c9744df64ee6dc173799b477a0d
yangli-io <yangli1990@live.com.au>
2021-01-06 17:26:53 +0800

    fix exponent

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,14 +6,18 @@ export let ONE_BI = BigInt.fromI32(1)
 export let ONE_BD = BigDecimal.fromString('1')
 export let BI_18 = BigInt.fromI32(18)
 
-export function exponent(decimals: BigInt): BigDecimal {
+export function exponent(base: BigDecimal, power: BigInt): BigDecimal {
   let bd = BigDecimal.fromString('1')
-  for (let i = ZERO_BI; i.lt(decimals as BigInt); i = i.plus(ONE_BI)) {
-    bd = bd.times(BigDecimal.fromString('10'))
+  for (let i = ZERO_BI; i.lt(power as BigInt); i = i.plus(ONE_BI)) {
+    bd = bd.times(base)
   }
   return bd
 }
 
+export function exponentTen(power: BigInt): BigDecimal {
+  return exponent(BigDecimal.fromString('10'), power);
+}
+
 export function convertToDecimal(value: BigInt, decimals: BigInt): BigDecimal {
-  return value.toBigDecimal().div(exponent(decimals))
+  return value.toBigDecimal().div(exponentTen(decimals))
 }
