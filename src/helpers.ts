da27a2cb2c1b96839e25787900dd0cbf68870ce6
yangli-io <yangli1990@live.com.au>
2021-01-06 16:43:03 +0800

    add more numbers

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,6 +3,8 @@ import { BigInt, BigDecimal } from "@graphprotocol/graph-ts"
 export let ZERO_BD = BigDecimal.fromString('0')
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)
 
 export function exponent(decimals: BigInt): BigDecimal {
   let bd = BigDecimal.fromString('1')
